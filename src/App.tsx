94456d97135c3dc8ea6c2e5f9ecfd7850605636a
st4rgut24 <gogocook25@gmail.com>
2022-09-10 19:16:22 -0700

    integrate put and get web3storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,55 @@
 import Story from "./artifacts/contracts/StoryShare.sol/Story.json";
 import StoryShare from "./artifacts/contracts/StoryShare.sol/StoryShare.json";
+import { Web3Storage } from 'web3.storage'
 
-import {Story as StoryType, StoryShare as StoryShareType} from "../../story_hat_backend/typechain-types"
-
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { CID } from 'multiformats/cid'
+import { Filelike } from "web3.storage/dist/src/lib/interface";
+import Web3Global from "./Web3Global";
 
 declare var window: any
 
 export default function App() {
+  // let web3Storage: Web3Storage;
+
+  const [storyText, setStoryText] = useState("")
+  const [file, setFile] = useState()
+  const [cid, setCid] = useState()
+  
+  function handleUploadChange(event: any) {
+    setFile(event.target.files[0])
+  }
+
+  function handleCidChange(event: any) {
+    console.log('set cid to', event.target.value);
+    setCid(event.target.value);
+  }  
+    
   useEffect(() => {
     connectWallet();
   }, []);
 
   const connectWallet = async () => {
-    try {
-      console.log('connect wallet');
-      const { ethereum } = window;
+    console.log('connect wallet');
+    // try {
+    //   console.log('connect wallet');
+    //   const { ethereum } = window;
 
-      if (!ethereum) {
-        alert("Please install MetaMask!");
-        return;
-      }
+    //   if (!ethereum) {
+    //     alert("Please install MetaMask!");
+    //     return;
+    //   }
 
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+    //   const accounts = await ethereum.request({
+    //     method: "eth_requestAccounts",
+    //   });
 
-      console.log("Connected", accounts[0]);
-      createStory();
-    } catch (error) {
-      console.log(error);
-    }
+    //   console.log("Connected", accounts[0]);
+    //   // createStory();
+    // } catch (error) {
+    //   console.log(error);
+    // }
   };
 
   const createStory = async () => {
@@ -77,5 +94,78 @@ export default function App() {
     console.log("retrieved the story: " + storyAddr);
   };
 
-  return <div></div>;
+  function handlePostWeb3(event: any) {
+    console.log('submitting file');
+    event.preventDefault()
+    if (file){
+      storeFiles([file]);
+    }
+    else {
+      console.error("no file was chosen for submission");
+    }
+  }
+
+  function handleGetWeb3(event: any) {
+    console.log('retrieving a file with cid', cid);
+    event.preventDefault();
+    if (cid) {
+      retrieve(cid);
+    }
+  }  
+
+  async function retrieve (cid: string) {
+    if (Web3Global.web3Storage) {
+      const res = await Web3Global.web3Storage.get(cid)
+      if (res) {
+        console.log(`Got a response! [${res.status}] ${res.statusText}`);
+        let files = await res.files();
+        if (!res.ok) {
+          throw new Error(`failed to get ${cid}`);
+        }
+        else if (files.length > 0) {
+          let file = files[0];
+          // request succeeded! do something with the response object here...
+          console.log(`${file.cid} -- ${file.size}`)
+          let storyText = await file.text();
+          console.log(storyText);
+          
+        }
+      }
+    }
+  }
+
+  function setStory(storyText: string) {
+    setStoryText(storyText);
+  }
+
+  async function storeFiles (files: Iterable<Filelike>): Promise<string | void> {
+    console.log('making storage client...');
+    if (Web3Global.web3Storage) {
+      console.log('storing files...');
+      const cid = await Web3Global.web3Storage.put(files)
+      console.log('stored files with cid:', cid)
+      return cid
+    }
+    else {
+      console.log('could not create storage client');
+    }
+  }
+
+  return (
+    <div className="App">
+        <div id="storyBox">{storyText}</div>
+        <br/>
+        <form onSubmit={handlePostWeb3}>
+          <h1>Upload File</h1>
+          <input type="file" onChange={handleUploadChange}/>
+          <button type="submit">Upload</button>
+        </form>
+        <br/>
+        <form onSubmit={handleGetWeb3}>
+          <h1>Retrieve File</h1>
+          <input type="text" onChange={handleCidChange}/>
+          <button type="submit">Retrieve</button>
+        </form>
+    </div>
+  )
 }
---
+++ src/Web3Global.ts
@@ -0,0 +1,15 @@
+import { Web3Storage } from 'web3.storage'
+
+const WEB3_STORAGE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDBmREFEQTRGNGRhNDM2NDYxNDZkQzZhNjBEZWFkOEVENTgwYzRFNmUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjI4NDkxNDgyMzMsIm5hbWUiOiJzdG9yeV9oYXQifQ.qJ7AlI3OeVjMQkSOymIQCuXO5vlsE_Baw0dHAZQTPgY";
+
+class Web3Global {
+    web3Storage: Web3Storage;
+
+    constructor(){
+        let w3s = new Web3Storage({token: WEB3_STORAGE_API_KEY});
+
+        this.web3Storage = new Web3Storage({token:WEB3_STORAGE_API_KEY})
+    }
+}
+
+export default new Web3Global();
